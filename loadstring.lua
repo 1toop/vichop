6024357be16b47660aa7d5e4909968743c3c6c69
toop <173619288+1toop@users.noreply.github.com>
2025-12-03 21:48:40 +0300

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,3 @@
+_G.Webhook = "" -- put your webhook here (optional)
+_G.UserId = "" -- put your discord id here for ping (optional)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1toop/vichop/refs/heads/main/hop.lua"))()
